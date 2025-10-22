c738bfb9530a72802b39ee5e9c32fbd4d2b08d62
JanTrnavsky <jan.trnavsky96@gmail.com>
2025-10-22 10:31:11 +0200

    lesson 3

--- src/tests/example.spec.js
+++ src/tests/example.spec.js
@@ -15,3 +15,30 @@ test("lesson 2", async ({ page }) => {
     await page.getByLabel("Email").screenshot({ path: "by_label.png"});
     console.log(await page.title());
 });
+
+test("lesson 3", async ({ page }) => {
+    await page.goto("/prihlaseni");
+    const headingLocator = page.getByRole("heading", {level: 1});
+    const headingText = await headingLocator.textContent();
+    console.log(headingText);
+    const emailField = page.getByLabel("Email");
+    console.log("Is email field visible? " + await emailField.isVisible());
+    console.log("Is email field enabled?" + await emailField.isEnabled());
+
+    const forgotPassword = page.getByText("Zapomněli jste své heslo?");
+    console.log("Forgot passoword href: " + await forgotPassword.getAttribute("href"));
+
+    const passwordField = page.getByLabel("Heslo");
+    console.log("Je videt? " + await passwordField.isVisible());
+    console.log("Je povoleno? " + await passwordField.isEnabled());
+
+    const loginButton = page.getByRole('button', { name: 'Přihlásit' });
+    console.log("Text: " + await loginButton.textContent());
+
+    await emailField.fill("da-app.admin@czechitas.cz");
+    await passwordField.fill("Czechitas123");
+    await loginButton.click();
+
+    const userName = page.locator(".navbar-right").locator("strong");
+    console.log("Logged user: " + await userName.textContent());
+});
